c3d373cfd335cdab38eb7b01b2e8b055122e0472
Oliver <nils-oliver.schliebs@uni-tuebingen.de>
2016-02-02 21:09:09 +0100

    Minor Updates

--- src/GUI/PDB123PresenterStructure.java
+++ src/GUI/PDB123PresenterStructure.java
@@ -1,4 +1,4 @@
-package TertStructure.PDB3D;
+package GUI;
 
 import SecStructure.RNA2D.Rna2DEdge;
 import SecStructure.RNA2D.Rna2DNode;
@@ -13,19 +13,20 @@ import java.util.Map;
 
 /**
  * Created by oliver on 29.01.16.
- * Main class for the assembly of an RNA 3D sequence
- * structure from its individual nucleotides
+ * Main class for the assembly of  RNA 1D 2D and 3D representation
+ * Coordinates are taken from ntMap, containing a PDBNucleotide for
+ * every index position
  */
-public class Rna3DStructure
+public class PDB123PresenterStructure
 {
-    private Group structure;
+    private Group structure3D;
 
-    public Rna3DStructure(Group structure) {
-        this.structure = structure;
+    public PDB123PresenterStructure(Group structure) {
+        this.structure3D = structure;
     }
     public void generate3DStructure(int firstNtIndex, int lastNtIndex, String colorMode, Map<Integer, PDBNucleotide> ntMap, BooleanProperty showBackbone, BooleanProperty showSugar, BooleanProperty showNucleoBase, ArrayList<Rna2DNode> rna2DNodes, ArrayList<Rna2DEdge> rna2DEdges){
-        // Clear previous structures
-        structure.getChildren().clear();
+        // Clear previous 3D structure
+        structure3D.getChildren().clear();
         // Store links to previous PDBNucleotide and its index position
         PDBNucleotide prev = null;
         int prevIndex = -10;
@@ -37,10 +38,12 @@ public class Rna3DStructure
             PDBNucleotide currentNt = ntMap.get(i);
             // Add structure from PDBNucleotide to subscene
             Group currStructure = currentNt.getStructure(showBackbone, showSugar, showNucleoBase);
-            structure.getChildren().add(currStructure);
+            currStructure.setOnMouseClicked(event -> System.out.println("Structure clicked"));
+            structure3D.getChildren().add(currStructure);
             // Set the colormode for the purin/pyrimidine ring
             currentNt.setColorMode(colorMode);
             // Initialize a 2D Node for 2D representation
+            // Store a reference to the 2D Node in PDBNucleotide
             Rna2DNode curr2DNode = new Rna2DNode(0.,0.,2,i, currentNt.isSelectedProperty());
             rna2DNodes.add(curr2DNode);
             currentNt.setRna2DNode(curr2DNode);
@@ -49,7 +52,7 @@ public class Rna3DStructure
                 DrawLine backboneConnection = new DrawLine(prev.getThreePrimeEnd(), currentNt.getFivePrimeEnd());
                 // Bind backbone visibility state to BooleanProperty showBackbone
                 backboneConnection.visibleProperty().bind(showBackbone);
-                structure.getChildren().add(backboneConnection);
+                structure3D.getChildren().add(backboneConnection);
                 // Add 2D edge to 2D representation
                 Rna2DEdge currEdge = new Rna2DEdge(prev.getRna2DNode(), curr2DNode);
                 currEdge.setEdgeStyle("covBond");
